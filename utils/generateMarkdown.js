61741d13bd5685f3201cfd7a1325e182b83ee106
Ruben Franco <ruben.afranco@yahoo.com>
2023-02-20 20:26:06 -0800

    added markdown for readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -19,13 +19,58 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
+    
+  This project is liscensed under the ${license} license.`;
+  }
 
+  return 'None';
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ${renderLicenseBadge(data.license)}
 
+  ## Description
+  ${data.desscription}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+
+  - [Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  - [License](#license)
+
+  - [Contributions](#contributions)
+
+  - [Tests](#tests)
+
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributions
+
+  ${data.contributors}
+
+  ## Tests
+
+  ${data.test}
+
+  ##Questions
+  
+  For any questions you may have, please emal me at ${data.email}.
+  You can view mor eof my work at https://github.com/${data.github} .
 `;
 }
 
